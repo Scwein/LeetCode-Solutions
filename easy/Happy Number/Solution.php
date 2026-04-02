80ca5e9b48944a604c01a6d466847e1e227297d0
Scwein <danila20102005@gmail.com>
2026-04-02 23:11:54 +0300

    added happy number solution

---
+++ easy/Happy Number/Solution.php
@@ -0,0 +1,31 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param Integer $n
+     * @return Boolean
+     */
+    function isHappy($n) {
+        $seen = [];
+        
+        while (true) {
+            $sum = 0;
+            $s = (string) $n;
+            
+            for ($i = 0; $i < strlen($s); $i++) {
+                $sum += $s[$i]**2;
+            }
+            
+            if ($sum == 1) {
+                return true;
+            }
+            if (isset($seen[$sum])) {
+                return false;
+            }
+            
+            $seen[$sum] = true;
+            $n = $sum;
+        }
+    }
+}
